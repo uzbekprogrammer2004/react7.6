7bdd96bc60184bc5a549dadbb5e9ace1b02e742c
uzbekprogrammer2004 <asilbeksnetlify@gmail.com>
2024-07-08 20:32:29 +0500

    werdsd

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 
-const index = () => {
+const Index = () => {
   return (
     <div>Bu Home qismi "Xizmatlar" ga o'ting </div>
   )
 }
 
-export default index
+export default Index
--- src/service/auth.js
+++ src/service/auth.js
@@ -1,9 +1,9 @@
 import http from './config'
 const auth = {
-    sign_in: (data)=> http.post("/auth/login",data),
-    sign_up: (data)=> http.post("/auth/register", data),
-    auth_verify: (data)=> http.post("/auth/verify", data),
-    forgot_password: (data)=> http.post("/auth/forgot-password", data),
-    update_password: (data)=> http.post("/auth/verify-forgot-password", data)
+    sign_in: (data)=> http.post("/login",data),
+    sign_up: (data)=> http.post("/register", data),
+    auth_verify: (data)=> http.post("/verify", data),
+    forgot_password: (data)=> http.post("/forgot", data),
+    update_password: (data)=> http.post("/update-password", data)
 }
 export default auth
